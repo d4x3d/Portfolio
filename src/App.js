e2c50bf68955e41f3ceda03d12d53b05ad32dafb
Alaa Alaff <travel98_alaff@hotmail.com>
2021-11-23 04:00:53 +0300

    start MainBody Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import "./App.scss";
+import "./assets/Main.scss";
 
 import SocialMediaContainer from "./components/SocialMediaContainer/SocialMediaContainer";
+import MainBody from "./components/MainBody/MainBody";
 
 function App() {
   return (
     <div className="App">
       <SocialMediaContainer />
+      <MainBody />
     </div>
   );
 }
---
+++ src/components/MainBody/MainBody.js
@@ -0,0 +1,16 @@
+import "./MainBody.scss";
+function MainBody() {
+  return (
+    <div>
+      <div className="main-title">
+        <h1>
+          Hello I’m <span className="roteted">Alaa</span> ,
+          <br />
+          Front-End Web Developer
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default MainBody;
